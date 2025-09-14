5c82f056a1983975901b7339aace6af9558fe8a1
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-14 10:45:43 +0900

    コントローラー、マイグレーションの作成

--- src/database/migrations/2025_09_05_230500_create_shipments_table.php
+++ src/database/migrations/2025_09_05_230500_create_shipments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateShipmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shipments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('shipment_postcode');
+            $table->string('shipment_address');
+            $table->string('shipment_building');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shipments');
     }
 }
