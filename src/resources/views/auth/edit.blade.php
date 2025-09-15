25a8f791b699929205918b8aad409dc6dd98aedd
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-15 23:07:49 +0900

    プロフィールビューのたたき台作成

---
+++ src/resources/views/auth/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/edit.css') }}">
+@endsection
+
+@section('content')
+<div class="content-wrapper">
+    <div class="content-title">
+        <h2>プロフィール設定</h2>
+    </div>
+    <div class="edit-form__wrapper">
+        <form action="{{ route('') }}" class="form-group" method="post">
+            @csrf
+            <div class="profile__img-avator">
+
+            </div>
+            <div class="form-group">
+                <div class="form-group__title">
+                    <p>ユーザー名</p>
+                </div>
+                <div class="form-group__content">
+                    <div class="form-group__input-text">
+                        <input class="form__input" type="text" name="name" />
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group__title">
+                    <p>郵便番号</p>
+                </div>
+                <div class="form-group__content">
+                    <div class="form-group__input-text">
+                        <input class="form__input"type="text" name="postcode" />
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group__title">
+                    <p>住所</p>
+                </div>
+                <div class="form-group__content">
+                    <div class="form-group__input-text">
+                        <input class="form__input"type="text" name="address" />
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group__title">
+                    <p>建物名</p>
+                </div>
+                <div class="form-group__content">
+                    <div class="form-group__input-text">
+                        <input class="form__input"type="text" name="building" />
+                    </div>
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit"type="submit">更新する</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
