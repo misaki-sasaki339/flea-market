dafaa5147b6bcc669a9cf5c70890e0405f146e57
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-15 17:51:35 +0900

    共通レイアウトの作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.default')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('layouts/app.css') }}">
+
+@section('header')
+<nav class="header__nav-wrapper">
+    <div class="header__item-search">
+        <form action="{{ route('search') }}" class="search-form" method="get">
+            @csrf
+            <input type="text" name="keyword" placeholder="なにをお探しですか？" value="{{ request('keyword') }}" />
+        </form>
+    </div>
+    <ul class="nav-list">
+        @auth
+        <li><a href="{{ route('logout') }}">ログアウト</a></li>
+        <li><a href="{{ route('mypage') }}">マイページ</a></li>
+        <li><a href="{{ route('sell') }}">出品</a></li>
+        @endauth
+
+        @guest
+        <li><a href="{{ route('login') }}">ログイン</a></li>
+        <li><a href="{{ route('login') }}">マイページ</a></li>
+        <li><a href="{{ route('login') }}">出品</a></li>
+        @endguest
+    </ul>
+</nav>
