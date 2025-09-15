be9fbf221e2e5e3c6982b67d4e7e30325d4eda9e
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-15 15:07:37 +0900

    非会員ビューのたたき台作成

--- src/resources/views/public/exhibition.blade.php
+++ src/resources/views/public/exhibition.blade.php
@@ -6,5 +6,17 @@
 
 @section('content')
 <div class="content">
-    
+    <div class="tab-switch">
+        <label><input type="radio" name="tab" checked />おすすめ</label>
+        <!--下線表示の方法検討-->
+        <div class="tab-content">
+            <!--@foreachで表示-->
+        </div>
+
+        <label><input type="radio" name="tab" />マイリスト</label>
+        <!--下線表示の方法検討-->
+        <div class="tab-content">
+            <!--favorite使って@foreach表示-->
+        </div>
+    </div>
 </div>
