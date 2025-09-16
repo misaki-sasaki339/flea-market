c01c1251ce7b301e61b27e827473d2995b373e74
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-16 09:37:31 +0900

    sellビューの作成

---
+++ src/resources/views/auth/sell.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/sell.css') }}">
+@endsection
+
+@section('content')
+<div class="content__wrapper">
+    <div class="content__title">
+        <h2>商品の出品</h2>
+    </div>
+    <div class="sell-form__wrapper">
+
+    </div>
+</div>
+@endsection
