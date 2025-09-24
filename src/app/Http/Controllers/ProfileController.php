e08f54178471c1114dce457be5d0bdc860572f5d
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-24 23:52:06 +0900

    プロフィール編集のビュー、コントローラー作成

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class ProfileController extends Controller
 {
-    //
+    //プロフィール画面の表示
+    public function index(){
+        $user = Auth::user();
+        $sells = $user->sales()->orderBy('created_at', 'desc')->get();
+        $orders = $user->orders()->orderBy('created_at', 'desc')->get();
+        return view('mypage', compact('users'));
+    }
+
+    //プロフィールの編集画面の表示
+    public function edit(){
+        return view('mypage.edit');
+    }
+
+    //プロフィールの更新
+    public function update(ProfileRequest $request){
+        $user = $request->only(['name', 'postcode', 'address', 'building']);
+        User::find($request->id)->update($user);
+
+
+        return redirect('mypage');
+    }
 }
