d80b29b29f5aa4ea810930b64d9bc38a335f2351
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-25 23:27:44 +0900

    認証画面のビュー完成（レスポンシブ非対応）

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('auth/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 @endsection
 
 @section('title')
@@ -20,26 +20,28 @@
             </div>
             <div class="form-group__content">
                 <div class="form__input-text">
-                    <input class="form__input" type="text" name="email" />
+                    <input class="form__input @error('email') is-invalid @enderror" type="text" name="email" />
                     @error('email')
                     <p class="error-message">{{ $message }}</p>
                     @enderror
                 </div>
             </div>
+        </div>
+        <div class="form-group">
             <div class="form-group__title">
-                 <p class="form__label--item">パスワード</p>
+                <p class="form__label--item">パスワード</p>
             </div>
             <div class="form-group__content">
                 <div class="form__input-text">
-                    <input class="form__input" type="password" name="password" />
+                    <input class="form__input @error('password') is-invalid @enderror" type="password" name="password" />
                     @error('password')
                     <p class="error-message">{{ $message }}</p>
                     @enderror
-                </div>              
+                </div>
             </div>
-            <div class="form__button">
-                <button class="form__button-submit" type="submit">ログインする</button>
-            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">ログインする</button>
         </div>
     </form>
     <a class="content__link-register" href="{{ route('register') }}">会員登録はこちら</a>
