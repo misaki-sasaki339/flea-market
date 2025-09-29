136d733ff4ceb0f5e1ff281d5a0612098a20286f
misaki sasaki <misaki.morinaga03@gmail.com>
2025-09-30 00:16:30 +0900

    出品のMVCルーティング設定

--- src/app/Http/Controllers/SellController.php
+++ src/app/Http/Controllers/SellController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\Condition;
+use App\Models\Category;
+use App\Http\Requests\ExhibitionRequest;
 
 class SellController extends Controller
 {
-    //
+    public function create(){
+        $categories = Category::all();
+        $conditions = Condition::all();
+       
+        return view('auth.sell', compact('categories', 'conditions'));
+    }
+
+    public function store(ExhibitionRequest $request){
+        $items = $request->only('img', 'condition_id', 'name', 'brand', 'description', 'price');
+        $items['category_ids'] = $request->category_ids;
+
+        Item::create($items);
+        return redirect()->route('mypage');
+    }
 }
--- src/resources/views/auth/sell.blade.php
+++ src/resources/views/auth/sell.blade.php
@@ -1,101 +1,127 @@
 @extends('layouts.app')
 
 @section('css')
+@parent
 <link rel="stylesheet" href="{{ asset('css/auth/sell.css') }}">
 @endsection
 
 @section('title')
 <div class="title">
-    <h2>商品の出品</h2>
+    <p class="title__label">商品の出品</p>
 </div>
 @endsection
 
 @section('content')
 <div class="content">
-    <div class="sell-form">
-        <form class="form" action="{{ route('') }}" method="post">
-            @csrf
-            <div class="form-group__img">
+    <form class="form" action="{{ route('sell.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group__img">
+            <div class="form-group__title">
+                <p class="form__label--item">商品の画像</p>
+            </div>
+            <div class="form-group__content">
+                <div class="form-group__input-img">
+                    <label for="img" class="form__button-upload">画像を選択する</label>
+                    <input class="form__input-file" type="file" id="img" name="img" accept="image/png, image/jpeg" >
+                    @error('img')
+                    <p class="error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form-group__attributes">
+            <p class="form__label">商品の詳細</p>
+            <div class="form-group">
                 <div class="form-group__title">
-                    <h3>商品の画像</h3>
+                    <p class="form__label--item">カテゴリー</p>
+                </div>
+                <div class="form-group__content-category">
+                    @foreach($categories as $category)
+                    <label class="form__item-checkbox">
+                        <input class="form__input-checkbox" type="checkbox" name="category_ids[]" value="{{ $category->id }}" {{ old('category_ids',[]) ? 'checked' : ''}} />
+                        <span class="checkbox-decoration">{{ $category->content }}</span>
+                    </label>
+                    @endforeach
+                    @error('category_ids')
+                    <p class="error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group__title">
+                    <p class="form__label--item">商品の状態</p>
                 </div>
                 <div class="form-group__content">
-                    <div class="form-group__input-img">
-                        <input class="form__input" type="file" name="img" accept="image/png, image/jpeg" required>
+                    <div class="form__item-select">
+                        <select class="form__item-select--option" name="condition_id" value="{{ old('condition_id') }}" >
+                            <option value="" disabled selected>選択してください</option>
+                            @foreach($conditions as $condition)
+                            <option value="{{ $condition->id }}" {{ old('condition_id') == $condition->id ? 'selected' : '' }}>{{ $condition->status}}</option>
+                            @endforeach
+                        </select>
+                        @error('condition_id')
+                        <p class="error-message">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
             </div>
-            <div class="form-group__attributes">
-                <h3>商品の詳細</h3>
+            <div class="form-group__desctiption">
+                <p class="form__label">商品名と説明</p>
                 <div class="form-group">
                     <div class="form-group__title">
-                        <p>カテゴリー</p>
+                        <p class="form__label--item">商品名</p>
                     </div>
                     <div class="form-group__content">
-                        <div class="form-group__input-xxxx">
-                            <input class="form__input" type="xxxx" name="category" />
+                        <div class="form-group__input-text">
+                            <input class="form__input" type="text" name="name" value="{{ old('name') }}" />
+                            @error('name')
+                            <p class="error-message">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="form-group__title">
-                        <p>商品の状態</p>
+                        <p class="form__label--item">ブランド名</p>
                     </div>
                     <div class="form-group__content">
-                        <div class="form-group__xxxx">
-
+                        <div class="form-group__input-text">
+                            <input class="form__input" type="text" name="brand" value="{{ old('brand') }}" />
                         </div>
                     </div>
                 </div>
-                <div class="form-group__desctiption">
-                    <h3>商品名と説明</h3>
-                    <div class="form-group">
-                        <div class="form-group__title">
-                            <p>商品名</p>
-                        </div>
-                        <div class="form-group__content">
-                            <div class="form-group__input-text">
-                                <input class="form__input" type="text" name="name" />
-                            </div>
+                <div class="form-group">
+                    <div class="form-group__title">
+                        <p class="form__label--item">商品の説明</p>
+                    </div>
+                    <div class="form-group__content">
+                        <div class="form-group__input-textarea">
+                            <textarea name="description" class="form__input-textarea">{{ old('description') }}</textarea>
+                            @error('description')
+                            <p class="error-message">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="form-group__title">
-                            <p>ブランド名</p>
-                        </div>
-                        <div class="form-group__content">
-                            <div class="form-group__input-text">
-                                <input class="form__input" type="text" name="brand" />
-                            </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-group__title">
+                        <p class="form__label--item">販売価格</p>
+                    </div>
+                    <div class="form-group__content">
+                        <div class="form-group__input-text">
+                            <input class="form__input" type="text" name="price" value="{{ old('price') }}"/>
+                            @error('price')
+                            <p class="error-message">{{ $message }}</p>
+                            @enderror
+                            <!--￥マークの表示を疑似要素使うかJS使うか検討-->
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="form-group__title">
-                            <p>商品の説明</p>
-                        </div>
-                        <div class="form-group__content">
-                            <div class="form-group__input-textarea">
-                                <textarea name="description" class="form__input"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="form-group__title">
-                            <p>販売価格</p>
-                        </div>
-                        <div class="form-group__content">
-                            <div class="form-group__input-text">
-                                <input class="form__input" type="text" name="price" />
-                                <!--￥マークの表示を疑似要素使うかJS使うか検討-->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form__button">
-                        <button class="form__button-submit" type="submit">出品する</button>
-                    </div>
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">出品する</button>
                 </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 </div>
 @endsection
