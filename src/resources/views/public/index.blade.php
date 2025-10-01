b7f723b5a066b3b313eee647e92efefe2ba31f3e
misaki sasaki <misaki.morinaga03@gmail.com>
2025-10-01 20:12:55 +0900

    マイページおすすめ画面完成

--- src/resources/views/public/index.blade.php
+++ src/resources/views/public/index.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.app')
 
 @section('css')
+@parent
 <link rel="stylesheet" href="{{ asset('css/public/index.css') }}">
 @endsection
 
 @section('content')
 <div class="content">
-    <div class="sub-nav">
-        <a href="">おすすめ</a>
-        @auth
-        <a href="{{ route('mypage') }}">マイリスト</a>
-        @endauth
-
-        @guest
-        <a href="{{ route('login.create') }}">マイリスト</a>
-        @endguest
-    </div>
-
-    <div class="list-wrapper">
-
+    <div class="tab-wrapper">
+            <input class="tab__label-input" type="radio" name="tab_btn" id="tab1" checked>
+            <input class="tab__label-input" type="radio" name="tab_btn" id="tab2">
+        <div class="tab__label">
+            <label for="tab1">おすすめ</label>
+            <label for="tab2">マイリスト</label>
+        </div>
+        <div class="tab__content" id="content1">
+            @foreach($items as $item)
+            <div class="tab__content-item">
+                {{--<a href="{{ route('item.show'), $item }}"></a>--}}
+                <img src="{{ asset('storage/' . $item->img) }}" alt="商品画像">
+                <p class="item-name">{{ $item->name }}</p>
+            </div>
+            @endforeach
+        </div>
+        <div class="tab__content" id="content2">
+            <p>空白</p>
+        </div>
     </div>
 </div>
 @endsection
