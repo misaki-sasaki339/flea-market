22e649a698af44b2124f2c772df0a08791ec2b49
misaki sasaki <misaki.morinaga03@gmail.com>
2025-10-09 21:53:38 +0900

    購入画面の作成

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
 
 class PurchaseController extends Controller
 {
     //商品購入画面の表示
-    public function create(){
-
+    public function create(Item $item){
+        $user = Auth::user();
+        return view('auth.order.purchase', compact('item', 'user'));
     }
 
     //商品購入機能
